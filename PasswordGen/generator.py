b51656d0c184ed3abf7b7ca47f5f84bb7b2cd50d
unknown <joseeduardomj@gmail.com>
2023-06-25 17:06:55 -0300

    formatting according to pep8

--- PasswordGen/generator.py
+++ PasswordGen/generator.py
@@ -1,25 +1,12 @@
-#according to IBM, ascii goes from 33 to 126
+# according to IBM, ascii goes from 33 to 126
 
 from random import *
 
-#password_length = int(input("How long do you want your password to be? "))
-password_length = 14
-#chr returns the value in unicode / ascii
-#ord returns the value of the character in unicode / ascii
-def generatePassword(password_length = password_length):
-    #print('entering generator')
-    generatedPassword = []
+password_length = int(input("How long do you want your password to be? "))
+# password_length = 14
 
-    for char in range(password_length):
-        random_char = str(chr(randint(33,126)))
-        generatedPassword.append(random_char)
-    
-    formated_password =(''.join(generatedPassword))
-    return formated_password
-
-#print(generatedPassword)
-
-#print(chr(24))
+# chr returns the value in unicode / ascii
+# ord returns the value of the character in unicode / ascii
 
 numbers = [chr(i) for i in range(48, 58)]
 numbers = (''.join(numbers))
@@ -37,63 +24,61 @@ lower_case = (''.join(lower_case))
 upper_case = [chr(i) for i in range(65, 91)]
 upper_case = (''.join(upper_case))
 
-'''print('\nNumbers',numbers)
-print('\nSpecial Characters',special_characters)
-print('\nLower Case',lower_case)
-print('\nUpper Case',upper_case)
-print()'''
 
-def storePassword(password): #this function stores the password in a txt file
-    print('Storing password...')
-    with open('passwords.txt', 'a') as file:
-        file.write(password)
-        file.write("\n\n") #it adds a blank line between each password
+def generatePassword(password_length=password_length):
+    generatedPassword = []
+
+    for char in range(password_length):
+        random_char = str(chr(randint(33, 126)))  # a <= N <= b
+        generatedPassword.append(random_char)
+
+    formated_password = (''.join(generatedPassword))
+    return formated_password
+
 
 def isValidPassword(password):
     hasNumber = False
     hasSpecial = False
     hasLower = False
     hasUpper = False
-    numberCount = 0
-    specialCount = 0
-    lowerCount = 0
-    upperCount = 0
 
     for char in password:
         if char in numbers:
-            if numberCount < 1:
-                hasNumber = True
-                #print(char)
-                numberCount += 1
+            # print(char)
+            hasNumber = True
 
         if char in special_characters:
-            if specialCount < 1:
-                #print(char)
-                hasSpecial = True
-                specialCount += 1
+            # print(char)
+            hasSpecial = True
 
         if char in lower_case:
-            if lowerCount < 1:
-                #print(char)
-                hasLower = True
-                lowerCount += 1
+            # print(char)
+            hasLower = True
 
         if char in upper_case:
-            if upperCount < 1:
-                #print(char)
-                hasUpper = True
-                upperCount += 1
+            # print(char)
+            hasUpper = True
 
     if hasNumber and hasSpecial and hasLower and hasUpper:
-        #print('Password is valid')
+        # password is valid
         return password
     else:
-        #print('\n\n\nPassword is not valid')
-        new_password = generatePassword() #calling the function again to generate a new password
-        return isValidPassword(new_password) #using recursion to check if the new password is valid
+        # password is not valid
+        # calling the function again to generate a new password
+        new_password = generatePassword()
+        # using recursion to check if the new password is valid
+        return isValidPassword(new_password)
+
+
+def storePassword(password):  # this function stores the password in a txt file
+    print('Storing password...')
+    with open('passwords.txt', 'a') as file:
+        file.write(password)
+        file.write("\n\n")  # it adds a blank line between each password
+
 
 notCheckedPassword = generatePassword()
 password = isValidPassword(notCheckedPassword)
 
-print('Your pseudo-random password is: ', password)
+print(f'\nYour pseudo-random password is: {password}\n')
 storePassword(password)
