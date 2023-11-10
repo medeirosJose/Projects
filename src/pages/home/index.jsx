8043b7c8d6af876492fcc11d9f036c9d2598b481
José Eduardo <joseeduardomj@gmail.com>
2023-11-10 16:52:18 -0300

    backup 2

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,119 +1,152 @@
-import React, {useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { getMovieById, getMovieTrailer } from "../../server/api";
-import { RadioLanguages } from "./ratio";
-import { FormFloatingBasicExample } from "./ratio";
-import { BasicExample } from "./ratio";
-import { GenreList } from "../../server/api";
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { getMovieById, getMovieTrailer } from '../../server/moviesApi'
+import { RadioLanguages, SizesExample } from './ratio'
+import { FormFloatingBasicExample } from './ratio'
+import { BasicExample } from './ratio'
+import { GenreList } from '../../server/moviesApi'
+import { CloseButton, InputGroup } from 'react-bootstrap'
 
 function Home() {
-    document.title = "Home";
-    const [movieList, setMovieList] = useState();
-    const [movieTitle, setMovieTitle] = useState('');
-    const [movieInfo, setMovieInfo] = useState('');
-    const [language, setLanguage] = useState('');
-    const [trailerLink, setTrailerLink] = useState('');
-    const genreList = GenreList();
+  document.title = 'Home'
+  const [movieList, setMovieList] = useState()
+  const [movieTitle, setMovieTitle] = useState('')
+  const [movieInfo, setMovieInfo] = useState('')
+  const [language, setLanguage] = useState('')
+  const [trailerLink, setTrailerLink] = useState('')
+  const genreList = GenreList()
 
-    function searchMovie(title) {
-        setMovieTitle(title);
-        getMovieById(title, language ).then((response) => {
-            setMovieList(response.data);
-        });
-    }
-    
-    // converte os ids dos gêneros em nomes
-    function transformGenres(movieInfo, genreList) {
-      const genreIdToName = {};
-      genreList.forEach(genre => {
-        genreIdToName[genre.id] = genre.name;
-      });
-    
-      const genres = movieInfo.map(genreId => genreIdToName[genreId]);
-    
-      return genres;
-    }
+  function searchMovie(title) {
+    setMovieTitle(title)
+    getMovieById(title, language).then((response) => {
+      setMovieList(response.data)
+    })
+  }
 
-    async function selectMovie(movie) {
-      setMovieTitle(movie.title);
-      setMovieInfo(movie);
-    
-      const key = await getTrailerLink(movie.id);
-      if (key) {
-        setTrailerLink(key);
-      }
-      setMovieList();
-    }
-    
-    function handleLanguageChange(event) {
-      setLanguage(event.target.value);
-    }
+  // converte os ids dos gêneros em nomes
+  function transformGenres(movieInfo, genreList) {
+    const genreIdToName = {}
+    genreList.forEach((genre) => {
+      genreIdToName[genre.id] = genre.name
+    })
 
-    async function getTrailerLink(movieId) {
-      try {
-        const response = await getMovieTrailer(movieId);
-    
-        const trailers = response.data.results;
-        if (trailers.length > 0) {
-          // 1. Oficial = true, Name contém "Trailer", Type = "Trailer".
-          const firstPriorityTrailer = trailers.find(trailer => trailer.official && trailer.name.toLowerCase().includes('trailer') && trailer.type === 'Trailer');
-          if (firstPriorityTrailer) {
-            const key = firstPriorityTrailer.key;
-            console.log('Chave do vídeo (Primeira Prioridade):', key);
-            return key;
-          }
-    
-          // 2. Name contém "Trailer", Oficial = true.
-          const secondPriorityTrailer = trailers.find(trailer => trailer.name.toLowerCase().includes('trailer') && trailer.official);
-          if (secondPriorityTrailer) {
-            const key = secondPriorityTrailer.key;
-            console.log('Chave do vídeo (Segunda Prioridade):', key);
-            return key;
-          }
-    
-          // 3. Oficial = true, Type = "Trailer".
-          const thirdPriorityTrailer = trailers.find(trailer => trailer.official && trailer.type === 'Trailer');
-          if (thirdPriorityTrailer) {
-            const key = thirdPriorityTrailer.key;
-            console.log('Chave do vídeo (Terceira Prioridade):', key);
-            return key;
-          }
-    
-          console.log('Nenhum trailer com as prioridades encontradas para o filme com ID:', movieId);
-          return null;
-        } else {
-          console.log('Nenhum vídeo encontrado para o filme com ID:', movieId);
-          return null;
+    const genres = movieInfo.map((genreId) => genreIdToName[genreId])
+
+    return genres
+  }
+
+  async function selectMovie(movie) {
+    setMovieTitle(movie.title)
+    setMovieInfo(movie)
+
+    const key = await getTrailerLink(movie.id)
+    if (key) {
+      setTrailerLink(key)
+    }
+    setMovieList()
+  }
+
+  function handleLanguageChange(event) {
+    setLanguage(event.target.value)
+  }
+
+  async function getTrailerLink(movieId) {
+    try {
+      const response = await getMovieTrailer(movieId)
+
+      const trailers = response.data.results
+      if (trailers.length > 0) {
+        // 1. Oficial = true, Name contém "Trailer", Type = "Trailer".
+        const firstPriorityTrailer = trailers.find(
+          (trailer) => trailer.official && trailer.name.toLowerCase().includes('trailer') && trailer.type === 'Trailer'
+        )
+        if (firstPriorityTrailer) {
+          const key = firstPriorityTrailer.key
+          console.log('Chave do vídeo (Primeira Prioridade):', key)
+          return key
         }
-      } catch (error) {
-        console.error('Erro ao buscar o link do trailer:', error);
-        return null;
+
+        // 2. Name contém "Trailer", Oficial = true.
+        const secondPriorityTrailer = trailers.find(
+          (trailer) => trailer.name.toLowerCase().includes('trailer') && trailer.official
+        )
+        if (secondPriorityTrailer) {
+          const key = secondPriorityTrailer.key
+          console.log('Chave do vídeo (Segunda Prioridade):', key)
+          return key
+        }
+
+        // 3. Oficial = true, Type = "Trailer".
+        const thirdPriorityTrailer = trailers.find((trailer) => trailer.official && trailer.type === 'Trailer')
+        if (thirdPriorityTrailer) {
+          const key = thirdPriorityTrailer.key
+          console.log('Chave do vídeo (Terceira Prioridade):', key)
+          return key
+        }
+
+        console.log('Nenhum trailer com as prioridades encontradas para o filme com ID:', movieId)
+        return null
+      } else {
+        console.log('Nenhum vídeo encontrado para o filme com ID:', movieId)
+        return null
       }
+    } catch (error) {
+      console.error('Erro ao buscar o link do trailer:', error)
+      return null
     }
+  }
 
-  return (<div>
+  return (
+    <div>
+      <div style={{ padding: '2em' }}>
+        <h1>ÁREA DE TESTES</h1>
+        <hr />
+        <div style={{ border: '1px solid', borderColor: '#002e5b', padding: '2em', borderRadius: '16px' }}>
+          <p>Idioma de Busca</p>
+          <RadioLanguages language={language} onLanguageChange={handleLanguageChange} />
+        </div>
+        <div
+          style={{
+            border: '1px solid',
+            borderColor: '#002e5b',
+            padding: '2em',
+            borderRadius: '16px',
+            marginTop: '16px',
+          }}
+        >
+          <br />
+          <FormFloatingBasicExample
+            searchMovie={searchMovie}
+            movieList={movieList}
+            movieTitle={movieTitle}
+            selectMovie={selectMovie}
+          />
+          <br />
 
-    <div style={{ padding: "2em" }}>
-      <h1>ÁREA DE TESTES</h1>
-      <hr />
-      
-      <p>Idioma de Busca</p>
-      <RadioLanguages
-          language={language}
-          onLanguageChange={handleLanguageChange}
-        />
+          {/*console.log(movieInfo)*/}
+          {/* {movieInfo && console.log(transformGenres(movieInfo.genre_ids, genreList))} */}
+          <CloseButton />
+          <div className="area" style={{ display: 'flex' }}>
+            {movieInfo && (
+              <div style={{ flex: 1 }}>
+                <BasicExample
+                  img={movieInfo.poster_path}
+                  title={movieInfo.title}
+                  description={movieInfo.overview}
+                  trailer_link={trailerLink}
+                  genres={transformGenres(movieInfo.genre_ids, genreList)}
+                />
+              </div>
+            )}
 
-      <br />
-      <FormFloatingBasicExample searchMovie={searchMovie} movieList={movieList} movieTitle={movieTitle} selectMovie={selectMovie} />
-      <br />
-      
-      <p>Card</p>
-      {/*console.log(movieInfo)*/}
-      {/* {movieInfo && console.log(transformGenres(movieInfo.genre_ids, genreList))} */}
-      { movieInfo && <BasicExample img={movieInfo.poster_path} title={movieInfo.title} description={movieInfo.overview} trailer_link={trailerLink} genres={transformGenres(movieInfo.genre_ids, genreList)} />} 
+            <div style={{ flex: 1, marginLeft: '32px' }}>
+              <SizesExample />
+            </div>
+          </div>
+        </div>
       </div>
-  </div>);
-
+    </div>
+  )
 }
 
-export default Home;
+export default Home
--- src/pages/home/ratio.js
+++ src/pages/home/ratio.js
@@ -1,10 +1,11 @@
-import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { useState } from 'react';
-import './styles.css';
-import { Button, Card, Modal } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { useState } from 'react'
+import './styles.css'
+import { Button, Card, InputGroup, Modal } from 'react-bootstrap'
+import CloseButton from 'react-bootstrap/CloseButton'
 
 // usar essa funcao para criar um checkbox com duas opcoes
 // essas opcoes dirao qual o idioma usado para pesquisar um filme ✅
@@ -16,35 +17,39 @@ import { Button, Card, Modal } from 'react-bootstrap';
 // TROCAR O PLACEHOLDER DO CASO DE N TER IMAGEM ✅
 
 //* modal funcionando, porém talvez seja interessante mostrar somente o poster por completo
-// necessario alguns ajustes na estilização 
+// necessario alguns ajustes na estilização
 function BasicExample({ img, title, description, trailer_link, genres }) {
-  const [showModal, setShowModal] = useState(false);
-  const [modalImage, setModalImage] = useState('');
-  const [modalOverview, setModalOverview] = useState('');
+  const [showModal, setShowModal] = useState(false)
+  const [modalImage, setModalImage] = useState('')
+  const [modalOverview, setModalOverview] = useState('')
 
   const openModal = (image, overview) => {
-    setModalImage(image);
-    setModalOverview(overview);
-    setShowModal(true);
-  };
+    setModalImage(image)
+    setModalOverview(overview)
+    setShowModal(true)
+  }
 
   const closeModal = () => {
-    setShowModal(false);
-  };
+    setShowModal(false)
+  }
 
   return (
     <>
       <Card style={{ display: 'flex', width: '300px', height: 'auto' }}>
         <Card.Img
-          className='hoverImg'
-          variant='top'
-          src={img ? `https://image.tmdb.org/t/p/original/${img}` : 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'}
+          className="hoverImg"
+          variant="top"
+          src={
+            img
+              ? `https://image.tmdb.org/t/p/original/${img}`
+              : 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'
+          }
           onClick={() => openModal(img, description)}
         />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
-          <Button href={`https://www.youtube.com/watch?v=${trailer_link}`} target='_blank'>
+          <Button href={`https://www.youtube.com/watch?v=${trailer_link}`} target="_blank">
             Trailer
           </Button>
         </Card.Body>
@@ -62,13 +67,16 @@ function BasicExample({ img, title, description, trailer_link, genres }) {
           />
           <div style={{ textAlign: 'center', paddingTop: '8px' }}>
             {genres.map((genre, index) => (
-              <span key={index} style={{
-                display: "inline-block",
-                padding: "4px 8px",
-                borderRadius: "8px",
-                margin: "8px",
-                border: "1px solid #ccc",
-              }}>
+              <span
+                key={index}
+                style={{
+                  display: 'inline-block',
+                  padding: '4px 8px',
+                  borderRadius: '8px',
+                  margin: '8px',
+                  border: '1px solid #ccc',
+                }}
+              >
                 {genre}
               </span>
             ))}
@@ -77,11 +85,16 @@ function BasicExample({ img, title, description, trailer_link, genres }) {
         </Modal.Body>
       </Modal>
     </>
-  );
+  )
 }
 
+export { BasicExample }
 
-export {BasicExample};
+function Close() {
+  return <CloseButton />
+}
+
+export { Close }
 
 function RadioLanguages({ language, onLanguageChange }) {
   return (
@@ -113,90 +126,143 @@ function RadioLanguages({ language, onLanguageChange }) {
         </div>
       ))}
     </Form>
-  );
+  )
 }
-export { RadioLanguages };
+export { RadioLanguages }
 
+function SizesExample() {
+  const [comment, setComment] = useState('')
+  const maxCharCount = 500 // Defina o limite de caracteres desejado
+  const minHeight = '3em' // Defina a altura mínima desejada
+  const maxHeight = '10em' // Defina a altura máxima desejada
 
-function FormFloatingBasicExample({searchMovie, movieList, movieTitle, selectMovie}) {
-  const [nota, setNota] = useState(''); // Inicializa o estado da nota
+  const handleCommentChange = (e) => {
+    const inputComment = e.target.value
+    if (inputComment.length <= maxCharCount) {
+      setComment(inputComment)
+    }
+  }
+
+  const calculateTextAreaHeight = () => {
+    const lineHeight = 1.2 // Altura da linha estimada
+    const lines = Math.min(Math.ceil(comment.length / 30), maxCharCount / 30) // Estimativa do número de linhas com base na largura máxima de 30 caracteres por linha
+
+    const newHeight = `${Math.max(lines * lineHeight, parseFloat(minHeight))}em`
+    return newHeight
+  }
+
+  const isMaxCharReached = comment.length === maxCharCount
+  const charCountLabelColor = isMaxCharReached ? '#f79f8f' : 'inherit'
+
+  return (
+    <>
+      <FloatingLabel controlId="floatingTextarea" label="Sua crítica" className="mb-3">
+        <Form.Control
+          as="textarea"
+          placeholder="Leave a comment here"
+          value={comment}
+          onChange={handleCommentChange}
+          style={{ height: calculateTextAreaHeight(), maxHeight, paddingTop: '32px' }}
+        />
+      </FloatingLabel>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <div
+          style={{
+            border: '1px solid #00000040', // Adicione um estilo de borda
+            borderRadius: '8px', // Defina o raio da borda
+            padding: '4px', // Espaçamento interno
+          }}
+        >
+          <p style={{ color: charCountLabelColor, textAlign: 'right', margin: 0 }}>
+            {comment.length}/{maxCharCount}
+          </p>
+        </div>
+      </div>
+      <Button variant="primary">Publicar</Button>{' '}
+    </>
+  )
+}
+
+export default SizesExample
+
+export { SizesExample }
+
+function FormFloatingBasicExample({ searchMovie, movieList, movieTitle, selectMovie }) {
+  const [nota, setNota] = useState('') // Inicializa o estado da nota
 
   const handleNotaChange = (value) => {
     console.log('entrando no change')
-    setNota(value);
-  };
+    setNota(value)
+  }
 
   const handleNotaBlur = () => {
-    console.log('entrando no blur');
-    let notaWithPoint = nota.replace(/,/, ".");
-    notaWithPoint = parseFloat(notaWithPoint);
-    
+    console.log('entrando no blur')
+    let notaWithPoint = nota.replace(/,/, '.')
+    notaWithPoint = parseFloat(notaWithPoint)
+
     // faz o arrendondamento da nota
-    notaWithPoint = Math.round(notaWithPoint * 10) / 10;
-  
-    console.log(notaWithPoint);
-    setNota(notaWithPoint.toString());
-  };
+    notaWithPoint = Math.round(notaWithPoint * 10) / 10
+
+    console.log(notaWithPoint)
+    setNota(notaWithPoint.toString())
+  }
 
   // Verifica se a nota está fora do intervalo desejado
-  const isNotaInvalid = isNaN(nota) || nota < 0 || nota > 10;
+  const isNotaInvalid = isNaN(nota) || nota < 0 || nota > 10
 
   return (
     <Row className="g-2">
-      <Col md={9}>
+      <Col md={10}>
         <FloatingLabel controlId="floatingInputGrid" label="Escolha o filme">
-        <Form.Control
-          value = {movieTitle}
-          placeholder="Pesquise seu filme"
-          onChange = {e => searchMovie(e.target.value)}
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
-
+          <Form.Control
+            value={movieTitle}
+            placeholder="Pesquise seu filme"
+            onChange={(e) => searchMovie(e.target.value)}
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
         </FloatingLabel>
         {/* simula um dropdown menu */}
-        <div style={{ position: "absolute", zIndex: 10, background: 'white', padding: '10px' }}>
-      {movieList && movieList.results.slice(0, 7).map((movie, i) => (
-        <div
-          key={i}
-          onClick={(event) => selectMovie(movie)}
-          style={{
-            cursor: 'pointer',
-            padding: '5px',
-            transition: 'background-color 0.2s',
-            backgroundColor: 'transparent',
-          }}
-          onMouseEnter={(event) => {
-            event.target.style.backgroundColor = '#c4dcfc';
-          }}
-          onMouseLeave={(event) => {
-            event.target.style.backgroundColor = 'transparent';
-          }}
-        >
-          {movie.title}
+        <div style={{ position: 'absolute', zIndex: 10, background: 'white', padding: '10px' }}>
+          {movieList &&
+            movieList.results.slice(0, 7).map((movie, i) => (
+              <div
+                key={i}
+                onClick={(event) => selectMovie(movie)}
+                style={{
+                  cursor: 'pointer',
+                  padding: '5px',
+                  transition: 'background-color 0.2s',
+                  backgroundColor: 'transparent',
+                }}
+                onMouseEnter={(event) => {
+                  event.target.style.backgroundColor = '#c4dcfc'
+                }}
+                onMouseLeave={(event) => {
+                  event.target.style.backgroundColor = 'transparent'
+                }}
+              >
+                {movie.title}
+              </div>
+            ))}
         </div>
-      ))}
-    </div>
-
-          
       </Col>
-      <Col md={3}>
+      <Col md={2}>
         <FloatingLabel controlId="floatingSelectGrid" label="Nota">
-        <input
-        value={nota}
-        type='number' // type number impede que o usuario digite letras ou outros caracteres que não sejam numeros e virgula ou ponto
-        min='0'
-        max='10'
-        step='0.01'
-        onChange={(e) => handleNotaChange(e.target.value)}
-        onBlur={handleNotaBlur} 
-        className={`form-control ${isNotaInvalid ? 'is-invalid' : 'no-spinners'}`}
-      />
+          <input
+            value={nota}
+            type="number" // type number impede que o usuario digite letras ou outros caracteres que não sejam numeros e virgula ou ponto
+            min="0"
+            max="10"
+            step="0.01"
+            onChange={(e) => handleNotaChange(e.target.value)}
+            onBlur={handleNotaBlur}
+            className={`form-control ${isNotaInvalid ? 'is-invalid' : 'no-spinners'}`}
+          />
         </FloatingLabel>
       </Col>
     </Row>
-  );
+  )
 }
 
-export { FormFloatingBasicExample };
-
+export { FormFloatingBasicExample }
